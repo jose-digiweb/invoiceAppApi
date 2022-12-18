722342cd71019a375c86d6839fd9555af2147f67
Jose Furtado <josefurtado.digital@gmail.com>
2022-12-18 12:22:02 +0100

    Added basic express app config

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
 // Import dependencies
 import express, { Request, Response, NextFunction, Application } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 
+// Import modules
 import appRouter from './routes';
 
 // Initialize the express application
 export const app: Application = express();
+
+// Set cors policy
+app.use(cors());
+
+// Set support for json
+app.use(express.json());
+
+// Set the cookie parsing support
+app.use(cookieParser());
+
+// Configure url encoding
+app.use(express.urlencoded({ extended: true }));
+
+// Set the morgan logger
+app.use(morgan('tiny'));
 
 // Initialize the app router
 app.use(appRouter);
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 // Import dependencies
+import 'dotenv/config';
+
+// Import modules
 import { app } from './index';
 
+// The port our server will be listening on
+const port: number = parseInt(<string>process.env.PORT, 10);
+
 // Start the server
-app.listen(5000, () => console.log('App running on port 5000'));
+app.listen(port, () => console.log(`App running on port ${port}`));
